b79668379c1cc9e27ea4ba1af24526bc0b16b076
Julieta Garcia <julieta.garcia96@gmail.com>
2021-03-04 21:15:30 -0300

    ya está armada la función que determina quien gana

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-// En la pantalla principal aparecen primero las tres ipciones 
-//una vez seleccionada una se despliega una nueva parte de la pantalla en donde aparece tu elección y la de la compu 
-// siguiente paso figura en pantalla quien ganó y si querés jugar otra partida 
-//estados de la partida y puntajes 
-//Puntajes: ganador=2 perdedor=0 empate=1
-// determinar quien es el ganador se pueede hacer con varios IF y establenciendo los casos de partidas posibles 
-
-// Se me ocurre que para linkear imagánes son elecciones se puede armar un array con cada posibilidad
-// O puede ser un objeto que contenga la info de a quien gana o a quien vence
-
+// Elecciones posibles del juego, con sus respectivas imágenes y propiedades
 const rock ={
     name: "rock",
-    beats: "scissor",
+    beats: "scissors",
     lose: "papper",
     img: "./images/icon-rock.svg"
 }
@@ -27,46 +18,62 @@ const scissors ={
     img: "./images/icon-scissors.svg"
 }
 optionArray=[rock,paper,scissors];
-//
-playAgainButton= document.getElementById("play-again-button"); 
-// Get the modal
-const choiceModal = document.getElementById("choice-modal");
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
-playAgainButton.onclick = function() {
-  choiceModal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  choiceModal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == choiceModal) {
-    choiceModal.style.display = "none";
-  }
- }
-// esto no sé si lo necesito, CREO que no. 
-const paperIcon=document.getElementById("paper-icon");
-const scissorsIcon=document.getAnimations("scissors-icon");
-const rockIcon=document.getElementById("rock-icon");
-// Hacemos una escucha sobre todo el modal, según el elemento sobre el que se haga click, este se guarda en una variable para ser reutilizado 
-// tengo que controlar que al hacer click sobre el modal no me lo tome, ni me tome elbotón close -------------- ver luego
- choiceModal.addEventListener("click",(e)=>{
-    if (e.target==img){
-        // Guarda en una variable la elección del jugador
-        playerChoise=e.target;
-    }
-    return playerChoise
 
+playAgainButton= document.getElementById("play-again-button"); 
+// Tomamos el contenedor para que el juegador pueda elegir 
+const choiceContainer = document.getElementById("choice-container");
+playAgainButton.onclick = function() {
+  choiceContainer.style.display = "block";
+}
+// esto no sé si lo necesito, CREO que no. 
+const paperIcon=document.getElementById("icon-paper");
+const scissorsIcon=document.getElementById("icon-scissors");
+const rockIcon=document.getElementById("icon-rock");
+// Hacemos una escucha sobre todo el container, según el elemento sobre el que se haga click, este se guarda en una variable para ser reutilizado 
+let playerChoise;
+
+choiceContainer.addEventListener("click",(e)=>{ 
+    if (e.target==paperIcon){
+        // este if funciona!
+        playerChoise=paper;
+    } else if (e.target==rockIcon){
+        playerChoise=rock;
+    } else if (e.target==scissorsIcon){
+        playerChoise=scissors;
+    }
+    playerVsHouse ()
+    return playerChoise
 })
-// Ahora tenemos que mostrar en pantalla el elemento seleccionadp y tenemos que armar la funcion que nos muestre cual fue la eleccion de la página 
+
+// Armamos una función para que la página elija un elemento al azar
 
 function getRandomInt(min, max) {
     return num=Math.floor(Math.random() * (max - min)) + min;
 }
 getRandomInt(0,3);
 let houseChoise= optionArray[num];
-console.log(houseChoise)
-//
+
+// Ahora quiero mostrar en pantalla la elección del jugador y de la casa
+// y que se demore en motrar el resultado medio segundo
+// Además hay que armar la funcion que determina quien ganó! 
+let playerCount=0;
+let houseCount=0;
+
+// Una vez que tenemos la elección del jugador ejecutamos la competencia 
+//playerChoise vs houseChoise
+function playerVsHouse () {
+    console.log (`La casa eligió ${houseChoise.name} y el jugador eligió ${playerChoise.name}`)
+    if (playerChoise.beats==houseChoise.name){
+        console.log ("ganó el jugador")
+        playerCount ++;
+    // gana el player 
+    } else if (playerChoise.lose==houseChoise.name) {
+        console.log ("ganó la casa")
+        houseCount ++;
+    } else if (playerChoise.name==houseChoise.name){ 
+        console.log ("hubo empate");
+        playerCount ++;
+        houseCount ++;
+    }
+}
+    
