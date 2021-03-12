317cd8bdabada8e472a4bfe8b94805084aac1d02
Julieta Garcia <julieta.garcia96@gmail.com>
2021-03-12 03:06:40 -0300

    trabaje con las funciones, reordenandolas, complete el objeto paper, rock, scissors para usarlo en el html

--- script.js
+++ script.js
@@ -1,79 +1,187 @@
-// Elecciones posibles del juego, con sus respectivas imágenes y propiedades
+// Posbiles elecciones del juego con sus respectivas propiedades e íconos
 const rock ={
     name: "rock",
     beats: "scissors",
-    lose: "papper",
-    img: "./images/icon-rock.svg"
+    lose: "paper",
+    img: "./images/icon-rock.svg",
+    className: "rock-icon-container rock-border",
+    id: "rock-icon-container",
+    contentHTML: `<div class="icon-container rock-icon-container" id="rock-icon-container">
+    <img src="./images/icon-rock.svg" alt="Rock image" id="icon-rock">
+    </div>`
 }
 const paper ={
-    name: "papper",
+    name: "paper",
     beats: "rock",
     lose: "scissors",
-    img: "./images/icon-papper.svg"
+    img: "./images/icon-paper.svg",
+    className: "paper-icon-container paper-border",
+    id: "paper-icon-container",
+    contentHTML: `<div class="icon-container paper-icon-container" id="paper-icon-container">
+    <img src=" ./images/icon-paper.svg" alt="Paper imgae" id="icon-paper">
+    </div>`
 }
 const scissors ={
     name: "scissors",
-    beats: "papper",
+    beats: "paper",
     lose: "rock",
-    img: "./images/icon-scissors.svg"
+    img: "./images/icon-scissors.svg",
+    className: "scissors-icon-container scissors-border",
+    id: "scissors-icon-container",
+    contentHTML: `<div class="icon-container scissors-icon-container" id="scissors-icon-container">
+    <img src="./images/icon-scissors.svg" alt="Scissors image" id="icon-scissors">
+    </div>`
 }
-optionArray=[rock,paper,scissors];
+const optionArray=[rock,paper,scissors];
 
-playAgainButton= document.getElementById("play-again-button"); 
-// Tomamos el contenedor para que el juegador pueda elegir 
+// Creamos los contenedores de los íconos correspondientes a cada elemento
+const scissorsContainer = document.createElement ('div');
+scissorsContainer.innerHTML= `${scissors.contentHTML}`;
+
+const paperContainer=document.createElement('div');
+paperContainer.innerHTML=`${paper.contentHTML}`;
+
+const rockContainer=document.createElement('div');
+rockContainer.innerHTML=`${rock.contentHTML}`;
+
+// Construimos un contenedor para los íconos superiores que serán el papel y la tijera
+const topIcons =document.createElement('div');
+topIcons.className='top-icons';
+topIcons.appendChild(paperContainer);
+topIcons.appendChild(scissorsContainer);
+// Tomamos el contenedor de todos los elementos
 const choiceContainer = document.getElementById("choice-container");
-playAgainButton.onclick = function() {
-  choiceContainer.style.display = "block";
-}
-// esto no sé si lo necesito, CREO que no. 
-const paperIcon=document.getElementById("icon-paper");
-const scissorsIcon=document.getElementById("icon-scissors");
-const rockIcon=document.getElementById("icon-rock");
-// Hacemos una escucha sobre todo el container, según el elemento sobre el que se haga click, este se guarda en una variable para ser reutilizado 
+// Le agregamos los íconos superiores
+choiceContainer.appendChild (topIcons)
+// Le agregamos el ínono de la roca
+choiceContainer.appendChild (rockContainer)
+
 let playerChoise;
+let houseChoise;
+let playerScore=0;
 
-choiceContainer.addEventListener("click",(e)=>{ 
-    if (e.target==paperIcon){
-        // este if funciona!
-        playerChoise=paper;
-    } else if (e.target==rockIcon){
-        playerChoise=rock;
-    } else if (e.target==scissorsIcon){
-        playerChoise=scissors;
-    }
-    playerVsHouse ()
-    return playerChoise
-})
+// Tomamos los contenedores en donde mostraremos la elección del jugador por un lado y de la casa por el otro. Todo estó está dentro de la sección "results"
+const resultsContainer= document.getElementById ("results-container");
+const playerChoiseContainer=document.getElementById('player-choise-container');
+const houseChoiseContainer=document.getElementById("house-choise-container");
 
-// Armamos una función para que la página elija un elemento al azar
+// Creamos la sección en donde se mostrará al ganador 
+const winnerContainer=document.createElement('div');
+winnerContainer.innerHTML=`
+    <span id="win-lose"> </span>
+    <button id="play-again-button" class="play-again-button"> play again</button>
+    `
 
-function getRandomInt(min, max) {
-    return num=Math.floor(Math.random() * (max - min)) + min;
+// Modificamos el puntaje del jugador en pantalla
+const playerScoreText= document.getElementById('player-score');
+const completePlayerScoreText =(score)=>{
+    playerScoreText.textContent=score;
 }
-getRandomInt(0,3);
-let houseChoise= optionArray[num];
+// Inicializamos el contador en 0
+completePlayerScoreText(playerScore);
 
-// Ahora quiero mostrar en pantalla la elección del jugador y de la casa
-// y que se demore en motrar el resultado medio segundo
-// Además hay que armar la funcion que determina quien ganó! 
-let playerCount=0;
-let houseCount=0;
+// Armamos una función para que la app elija un número entero al azar
+function randomNum(min, max) {
+    num=Math.floor(Math.random() * (max - min)) + min;
+}
+let getHouseChoise =()=>{
+    randomNum(0,3)
+    houseChoise= optionArray[num];
+    return houseChoise
+}
 
-// Una vez que tenemos la elección del jugador ejecutamos la competencia 
-//playerChoise vs houseChoise
+let playerResult;
+
+
+
+// Función que determina quién ganó
 function playerVsHouse () {
     console.log (`La casa eligió ${houseChoise.name} y el jugador eligió ${playerChoise.name}`)
     if (playerChoise.beats==houseChoise.name){
-        console.log ("ganó el jugador")
-        playerCount ++;
+        console.log ("ganó el jugador");
+        playerResult="You win";
+        playerScore ++;
     // gana el player 
     } else if (playerChoise.lose==houseChoise.name) {
-        console.log ("ganó la casa")
-        houseCount ++;
+        console.log ("ganó la casa");
+        playerResult="You lose";
     } else if (playerChoise.name==houseChoise.name){ 
         console.log ("hubo empate");
-        playerCount ++;
-        houseCount ++;
+        playerResult="You lose";
+        playerScore ++;
+    }
+    completePlayerScoreText(playerScore);
+    resultsContainer.appendChild(winnerContainer);
+    winLose=document.getElementById("win-lose");
+    winLose.textContent=playerResult;
+    showButton()
+}
+
+// Tomamos los íconos de cada elemento para que luego el jugador pueda elegir
+const paperIcon=document.getElementById("icon-paper");
+const scissorsIcon=document.getElementById("icon-scissors");
+const rockIcon=document.getElementById("icon-rock");
+
+choiceContainer.addEventListener("click",(e)=>{ 
+    if (e.target==paperIcon){
+        playerChoiseIcon=paperContainer;
+        playerChoise=paper;
+    } else if (e.target==rockIcon){
+        playerChoise=rock;
+        playerChoiseIcon=rockContainer;
+
+    } else if (e.target==scissorsIcon){
+        playerChoise=scissors;
+        playerChoiseIcon=scissorsContainer;
+
+    }
+    // Una vez elegido el elemento llamamos a una función que nos mostrará esa elección en la pantalla
+    showResults();
+    return playerChoise,playerChoiseIcon
+});
+
+// Función que muestra en pantalla la elección de la casa
+const showHouseChoise=()=>{
+    houseChoiseContainer.innerHTML=`${houseChoise.contentHTML}`
+}
+
+const showResults =()=> {
+    choiceContainer.style.display="none";
+    resultsContainer.style.display="flex"
+    playerChoiseContainer.appendChild(playerChoiseIcon)
+
+    // Ejecutamos la función que nos devuelve la elección de la app
+    getHouseChoise();
+    // Demoramos 1 segundo que esa elección se muestre en pantalla
+    timeoutShowHouseChoise = setTimeout(showHouseChoise, 1000);
+    // DEmoramos dos segundos que se determine quién ganó
+    timeoutResults=window.setTimeout (playerVsHouse,2000);
+    // Demoramos dos 
+    //timeoutButton=window.setTimeout(showButton,2500)
+
+}
+// Función que muestra en pantalla el botón "Play again"
+const showButton =()=>{
+    // Función para volver a jugar
+    const playAgainButton= document.getElementById("play-again-button"); 
+    playAgainButton.onclick = function() {
+        // Se vuelve a mostrar en pantalla los tres elementos para elegir. Es importante volver a agregar al contenedor todos los elementos, porque al haber tomado el elemento seleccionado y reubicarlo en la sección de resultados, lo desplazamos de su lugar original.
+        choiceContainer.style.display = "flex";   
+        topIcons.appendChild(paperContainer);
+        topIcons.appendChild(scissorsContainer);
+        choiceContainer.appendChild (rockContainer)
+        // Se esconden los resultados
+        resultsContainer.style.display="none";
+     
+        houseChoiseContainer.innerHTML='';
+        playerChoiseContainer.innerHTML=`<span>You Picked</span>`
+        winLose.textContent=''
+        resultsContainer.removeChild(winnerContainer);
     }
 }
-    
+
+
+
+
+
+ 
